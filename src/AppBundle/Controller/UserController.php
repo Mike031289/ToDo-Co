796a87d1c4cd5f9cad880d6d499d2a64f21af44a
Mike Agbelou <mike.agbelou@gmail.com>
2026-07-16 22:49:59 +0200

    security(user): restrict user management access to administrators only
    - Restrict all user management paths (^/users) in app/config/security.yml access_control.
    - Add denyAccessUnlessGranted('ROLE_ADMIN') checks in all UserController actions for defense in depth.
    - Ensure only users holding administrative privileges can access user list, creation, and edition.

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,34 +4,57 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Class UserController
+ *
+ * Manages administrative user operations including list viewing, creation, and updating.
+ * Access to this entire controller is restricted strictly to users with ROLE_ADMIN.
+ *
+ * @package AppBundle\Controller
+ */
 class UserController extends Controller
 {
     /**
+     * Display the list of all registered users.
+     *
      * @Route("/users", name="user_list")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function listAction()
     {
+        // Restrict access to administrative users only
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         return $this->render('user/list.html.twig', [
             'users' => $this->getDoctrine()->getRepository('AppBundle:User')->findAll()
         ]);
     }
 
     /**
+     * Create and store a new user.
+     *
      * @Route("/users/create", name="user_create")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function createAction(Request $request)
     {
+        // Restrict access to administrative users only
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
 
         // Check if the form is submitted and valid before processing
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() === true && $form->isValid() === true) {
             $em = $this->getDoctrine()->getManager();
 
             // Encode the plain text password provided in the form
@@ -50,10 +73,19 @@ class UserController extends Controller
     }
 
     /**
+     * Edit an existing user's profile and roles.
+     *
      * @Route("/users/{id}/edit", name="user_edit")
+     *
+     * @param User $user
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function editAction(User $user, Request $request)
     {
+        // Restrict access to administrative users only
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         // Store the existing hashed password in case the password field is left blank
         $oldPassword = $user->getPassword();
 
@@ -61,10 +93,10 @@ class UserController extends Controller
         $form->handleRequest($request);
 
         // Check if the form is submitted and valid before processing
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() === true && $form->isValid() === true) {
 
             // Check if a new password has been filled in the form
-            if (!empty($user->getPassword())) {
+            if (empty($user->getPassword()) === false) {
                 // Encode and set the new password
                 $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
                 $user->setPassword($password);
